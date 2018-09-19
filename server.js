edb5c85bcf658d65e34485f76e3a2875455b4db4
przemek <wosin7@gmail.com>
2018-09-19 12:37:27 +0200

    zadanie

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+app.use(express.static('assets'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
+
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
